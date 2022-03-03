14ac8cca2330177f07faeb812d7082184fd56e54
Lohliaa <75283452+Lohliaa@users.noreply.github.com>
2022-03-04 00:58:44 +0700

    Add files via upload
    Ubah content

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
 
+
 <head>
 	<meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -10,13 +11,13 @@
 	<link href="{{asset('css/templatemo-style.css')}}" rel="stylesheet" />
 </head>
 <!--
-
+	
 Simple House
 
 https://templatemo.com/tm-539-simple-house
 
 -->
-<body> 
+<body style="background-color: #fff"> 
     @include('navbar')
     @yield('content')
     @include('footer')
